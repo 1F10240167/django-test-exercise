bc39290867b2d95e9a01fe67040cfc983ca731bf
1f10240252 <1f102402529@iniad.org>
2025-07-10 12:26:26 +0900

    修正3

--- todo/views.py
+++ todo/views.py
@@ -40,6 +40,7 @@ def close (request, task_id):
     task.completed = True
     task.save()
     return redirect(index)
+
 def delete(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
@@ -47,6 +48,7 @@ def delete(request, task_id):
         raise Http404('Task does not exist')
     task.delete()
     return redirect(index)
+
 def update(request,task_id):
     try:
         task=Task.objects.get(pk=task_id)
